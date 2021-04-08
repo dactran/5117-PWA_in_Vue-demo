8ca08ecdbcb3a484c394ecfb57c03ae1876fe634
Dac Tran <dactranhv99@gmail.com>
2021-04-07 19:07:35 -0500

    first commit. Setting up project with service worker + firebase

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    // ...other vue-cli plugin options...
+    pwa: {  
+      // configure the workbox plugin (either 'InjectManifest' or 'Generate SW')
+        workboxPluginMode: 'GenerateSW', 
+        workboxOptions: {
+            navigateFallback: '/index.html',
+            runtimeCaching: [
+                {
+                    urlPattern: new RegExp('^https://.+'),
+                    handler: 'networkFirst',
+                    options: {
+                        networkTimeoutSeconds: 20,
+                        cacheName: 'api-cache',
+                        cacheableResponse: {
+                            statuses: [0,200]
+                        }
+                    }
+                }
+            ]
+        }
+        //   workboxPluginMode: 'InjectManifest',
+        //   workboxOptions: {
+        //     // swSrc is required in InjectManifest mode.
+        //     swSrc: 'dev/sw.js',
+        //     // ...other Workbox options...
+        //   }
+    }
+  }
