2c982fba6b3cfedb3079f8150afa6be655989d8f
Hong <41907842+jaewon1013@users.noreply.github.com>
2021-04-12 15:44:53 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,30 @@
 module.exports = {
     // ...other vue-cli plugin options...
     pwa: {  
-      // configure the workbox plugin (either 'InjectManifest' or 'Generate SW')
-        workboxPluginMode: 'GenerateSW', 
+    //   configure the workbox plugin (either 'InjectManifest' or 'Generate SW')
+        // workboxPluginMode: 'GenerateSW', 
+        // workboxOptions: {
+        //     navigateFallback: '/index.html',
+        //     runtimeCaching: [
+        //         {
+        //             urlPattern: new RegExp('^https://.+'),
+        //             handler: 'networkFirst',
+        //             options: {
+        //                 networkTimeoutSeconds: 20,
+        //                 cacheName: 'api-cache',
+        //                 cacheableResponse: {
+        //                     statuses: [0,200]
+        //                 }
+        //             }
+        //         }
+        //     ]
+        // },
+        workboxPluginMode: 'InjectManifest',
         workboxOptions: {
-            navigateFallback: '/index.html',
-            runtimeCaching: [
-                {
-                    urlPattern: new RegExp('^https://.+'),
-                    handler: 'networkFirst',
-                    options: {
-                        networkTimeoutSeconds: 20,
-                        cacheName: 'api-cache',
-                        cacheableResponse: {
-                            statuses: [0,200]
-                        }
-                    }
-                }
-            ]
-        }
-        //   workboxPluginMode: 'InjectManifest',
-        //   workboxOptions: {
         //     // swSrc is required in InjectManifest mode.
-        //     swSrc: 'dev/sw.js',
+        swSrc: 'src/service-worker.js',
         //     // ...other Workbox options...
-        //   }
+        }
     }
   }
+
